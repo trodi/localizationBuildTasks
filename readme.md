f71ff936651d988dac9a83974adb4e324ffb5d68
trodi <troy.t.mckinnon@gmail.com>
2013-12-29 13:16:06 -0600

    initial project implementation commit

---
+++ readme.md
@@ -0,0 +1,25 @@
+localization-build-tasks
+====================
+A tool for preparing resources for translation and integrating them back into your product.
+
+Problem
+-------
+In a large code base, I've found that delivering resources to translators and integrating their work back into a product to be a pain point even though all my resources are in 
+properties files as they should be. You can give them your entire directory structure of resource files which may work the first time you translate your product, but I don't
+like the idea of relying on a third party to find and update translations the next next go around and I don't want to pay for them to re-translate an entire product. I needed a way 
+to:
+* compare the current state of resources with the state of the last translations
+* output what key/value pairs had been added/deleted/updated
+* import the updated translations into my localized resource files
+* integrate this into the build process
+
+How to Use
+----------
+1. 	Call from a build task just like any custom java ant build task.
+
+How to Develop
+--------------
+1.	Install Java 1.7 and set up JAVA_HOME
+2.	Install Scala 2.10.2 and set up SCALA_HOME
+3.	Install Ant, build with "compile" task
+4.	I imported into Scala IDE, use similar style 
---
+++ src/main/scala/LocalizationDiff.scala
@@ -0,0 +1,179 @@
+package main.scala
+
+import java.io.File
+import java.io.FileInputStream
+import java.util.Properties
+import scala.collection.JavaConverters
+import scala.collection.mutable.Map
+import scala.xml.NodeSeq.seqToNodeSeq
+import scala.xml.XML
+import scala.io.Source
+
+/**
+ * -run property extraction
+ * -send to translators
+ * -run property insertion
+ * 
+ * @author tmckinnon
+ */
+object LocalizationDiff {
+
+  val FILE_EXTENSION_PROPERTIES = ".properties"
+  val FILE_EXTENSION_RESX = ".resx"
+  val FILE_EXTENSION_TEMPLATE = ".ftl"
+  val ACCEPTED_FILE_EXTENSIONS = Set(FILE_EXTENSION_PROPERTIES, FILE_EXTENSION_RESX)
+  
+  def main(args: Array[String]) {
+
+    val (startDir_New, startDir_Old, exclustionList) = parseParameters(args)
+    var _addedKeys, _updatedKeys, _removedKeys, _wordCount = 0
+    
+    recurseFileSystem(startDir_New, true) //check for updates to existing files and additional files
+    recurseFileSystem(startDir_Old, false) //check for files removed in new set
+    
+    println(String.format("Total keys added[%s] changed [%s] deleted[%s]. ", _addedKeys.toString, _updatedKeys.toString, _removedKeys.toString))
+    println(String.format("Total word count [%s].", _wordCount.toString))
+    
+    /** Compares pFile from the new dir to the equivalent in the old dir. */
+    def helperCompareFiles(pFile: File) {
+        val _old = new File(pFile.getAbsolutePath().replace(startDir_New.getAbsolutePath(), startDir_Old.getAbsolutePath()))
+        if (_old.exists()) {
+        	updateCounters(compareFile(pFile, _old))  
+        } else {//handle new files
+        	val _propFile = JavaConverters.asScalaMapConverter(loadPropFile(pFile)).asScala
+        	updateCounters((_propFile.size,0,0,_propFile.size))
+        	println(new StringBuilder("// New Property File: ").append(pFile.getAbsolutePath()))
+          	printPropertySet("// New properties: ", _propFile) 
+        }
+	}
+	
+    /** Checks to see if pFile from the old dir was removed in the new dir. */
+	def helperCheckForRemovedFiles(pFile: File) {
+//    val func = { (pFile: File) => {
+        val _new = new File(pFile.getAbsolutePath().replace(startDir_Old.getAbsolutePath(), startDir_New.getAbsolutePath()))
+    	if (!_new.exists()) {
+	    	val _propFile = JavaConverters.asScalaMapConverter(loadPropFile(pFile)).asScala
+	    	updateCounters((0,0,_propFile.size,0))
+	    	println(new StringBuilder("// Removed Property File: ").append(pFile.getAbsolutePath()))
+	      	printPropertySet("// Removed properties: ", _propFile)
+    	}
+	}
+	
+    /** 
+     *  Recurse the File system at given point and print out comparisons of files. Call with both new dir and old dir.
+     *  Calling old dir will allow finding files that have been deleted since old version.
+     *  @param pIsNewDir - Whether the given dir is from the new version or not 
+     */
+    def recurseFileSystem(pDir: File, pIsNewDir: Boolean) {
+	    def helper(pFile: File) {
+	      if (pFile.isFile()) {
+	      	if (pIsNewDir) helperCompareFiles(pFile)
+	      	else helperCheckForRemovedFiles(pFile)
+	      } else if (pFile.isDirectory()) {
+	        recurseFileSystem(pFile, pIsNewDir)
+	      }
+	    }
+	    
+    	pDir.listFiles.toSet[File].filter(
+    	    x => (x.isDirectory() || isAcceptedFileType(x)) && isNotExcluded(x)).map(x => helper(x))
+    }
+    
+    /** @return Whether the file is allowed or in the exclusion list. */
+    def isNotExcluded(pFile: File): Boolean = {
+      val _name = pFile.getName();
+      !exclustionList.contains(_name)
+    }
+    
+    /**
+     * Addes the passed in values to the existing counts.
+     * @param (added key pairs, updated key pairs, removed key pairs, total wc to send to translators)
+     */
+	def updateCounters(pCounters: (Int,Int,Int,Int)) {
+		_addedKeys += pCounters._1
+		_updatedKeys += pCounters._2
+		_removedKeys += pCounters._3
+		_wordCount += pCounters._4
+	}     
+  }
+  
+  /** @return Whether this file type is supported for properties file. */
+  private def isAcceptedFileType(pFile: File): Boolean = {
+  	ACCEPTED_FILE_EXTENSIONS.map(x => pFile.getName().contains(x)).contains(true)
+  }
+      
+  /**
+   * @param 0 - start dir of new resources
+   * @param 1 - start dir of old resources (extracted and unzipped from older build for now)
+   * @param 2 - ???
+   */
+  private def parseParameters(args: Array[String]): (File, File, Set[String]) = {
+    val _args = (new File(args(0)), new File(args(1)), args(2).split(',').toSet[String])
+    
+    if (!_args._1.exists()) throw new IllegalArgumentException("New starting dir doesn't exist!")
+    if (_args._1.isFile()) throw new IllegalArgumentException("New starting dir is a file!")
+    if (!_args._2.exists()) throw new IllegalArgumentException("Old starting dir doesn't exist!")
+    if (_args._2.isFile()) throw new IllegalArgumentException("Old starting dir is a file!")
+    
+    _args
+  }
+  
+  private def loadPropFile(pFile: File): Properties = {
+    if (pFile.getName().contains(FILE_EXTENSION_PROPERTIES)) loadJavaPropFile(pFile)
+    else if (pFile.getName().contains(FILE_EXTENSION_RESX)) loadXMLPropFile(pFile)
+    else throw new IllegalArgumentException("Can't read file type: " + pFile.getName())
+  }
+  
+  /** Load in properties from Java properties file. */
+  private def loadJavaPropFile(pFile: File): Properties = {
+  	val _propFile = new Properties
+  	val _inputStream = new FileInputStream(pFile)
+  	_propFile.load(_inputStream)
+  	return _propFile
+  }
+  
+  /** Load in properties file from .resx file. */
+  private def loadXMLPropFile(pFile: File): Properties = {
+    val _node = "data"
+    val _key = "name"
+    val _value = "value"
+    val _propFile = new Properties
+    val _xmlObject = XML.loadFile(pFile).child.filter(x => x.label == _node)
+    	.map(x => (_propFile.setProperty(x.attribute(_key).get.toString(), x.child.filter(x => x.label == _value).text)))
+    //printPropertySet("test OAI file", _propFile.stringPropertyNames().toArray().toSet, _propFile)
+	return _propFile
+  }
+  
+  /** @return (added, updated, removed, total word count that has to be sent to translators) - tuple of the count of changed properties. */
+  private def compareFile(pNewFile: File, pOldFile: File): (Int, Int, Int, Int) = {
+    println(new StringBuilder("// Property File: ").append(pNewFile.getName()))
+    
+    val _new = loadPropFile(pNewFile)
+    val _old = loadPropFile(pOldFile)
+    
+    val _newKeysSet = JavaConverters.asScalaMapConverter(_new).asScala
+    val _oldKeysSet = JavaConverters.asScalaMapConverter(_old).asScala
+    
+    val _addedKeys = _newKeysSet.filter(x => !_oldKeysSet.contains(x._1))
+	printPropertySet("// New properties: ", _addedKeys)
+    
+    val _removedKeys = _oldKeysSet.filter(x => !_newKeysSet.contains(x._1))
+	printPropertySet("// Removed properties: ", _removedKeys)
+	
+	val _updatedValues = _newKeysSet.filter(x => _oldKeysSet.contains(x._1) && _oldKeysSet(x._1) != _newKeysSet(x._1))
+	printPropertySet("// Updated properties: ", _updatedValues)
+	
+	val _wordCount = _addedKeys.map(x => x._2.split(' ').size).sum + _updatedValues.map(x => x._2.split(' ').size).sum
+	
+	return (_addedKeys.size, _updatedValues.size, _removedKeys.size, _wordCount)
+  }
+  
+  /** Print out a prettily formatted block of properties associated with pHeader. */
+  private def printPropertySet(pHeader: String, pSet: Map[String,String]) {
+    def format(pKey: String, pValue: String) {
+      println(new StringBuilder("\t").append(pKey).append('=').append(pValue).toString())
+    }
+    if (!pSet.isEmpty) println(pHeader)
+    pSet.map(x => format(x._1, x._2))
+  }
+
+}
